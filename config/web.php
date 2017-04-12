a1c5788badc9f7d47ebdf26ed9444cf082abb99d
altarfius <altarfius@gmail.com>
2017-04-12 17:27:31 +0300

    Изменения языка

--- config/web.php
+++ config/web.php
@@ -3,7 +3,8 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
+    'id' => 'eprint',
+	'language' => 'ru-RU',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
